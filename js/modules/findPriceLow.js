998a2532401b77d9c437376f8ae2ef530e74a808
Wesley Anjos <wanjos.andrade@gmail.com>
2021-04-20 14:17:23 -0300

    Implementado busca por cor mais barata de acordo com versão do carro

---
+++ js/modules/findPriceLow.js
@@ -0,0 +1,46 @@
+export default function findPriceLow(colorsAvailable) {
+  const infoPrice = document.querySelector(".infoPrice");
+  const nameColorSelected = document.querySelector('.nameColorSelected')
+
+  const ufSaved = localStorage.getItem("HONDA_LOCAL_STATE");
+  const infoColorsAndStates = [];
+  const nameColorAndPrice = [];
+
+  colorsAvailable.forEach((info) => {
+    infoColorsAndStates.push({
+      name: info.name,
+      states: info.pps,
+    });
+  });
+
+  infoColorsAndStates.forEach((element) => {
+    Object.entries(element.states).forEach((state) => {
+      if (state[0] === ufSaved) {
+        nameColorAndPrice.push({
+          nameColor: element.name,
+          priceColor: Number(state[1].field_pps_preco),
+        });
+      }
+    });
+  });
+
+  const lowestPrice = nameColorAndPrice.reduce((accumulator, info) => {
+    return accumulator < info.priceColor ? accumulator : info.priceColor;
+  });
+
+  console.log(lowestPrice);
+
+  const colorNameWithLowPrice = nameColorAndPrice.find(
+    (infoCar) => infoCar.priceColor === lowestPrice
+  );
+
+  const priceConverted = colorNameWithLowPrice.priceColor.toLocaleString(
+    "pt-br",
+    { style: "currency", currency: "BRL" }
+  );
+
+  infoPrice.innerHTML = `<span>A partir de <p class="priceState">${priceConverted}</p></span>`
+  nameColorSelected.innerText = colorNameWithLowPrice.nameColor
+
+  // console.log(nameColorAndPrice);
+}
